cade063c00a7c2ecc06484daa773560f65c23527
LinuxGit <gofaceme@gmail.com>
2012-12-02 22:27:06 +0800

    add user micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,27 +2,27 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:edit, :update, :index, :destroy]
   before_filter :correct_user, only: [:edit, :update]
   before_filter :admin_user, only: :destroy
-  before_filter :signined_user, only: [:new, :create]
+  before_filter :prevent_signined_resignup, only: [:new, :create]
 
   def new
 	  @user = User.new
   end
 
   def show
-	  @user = User.find(params[:id])
+    @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
-	end
+  end
 
   def create
-	  @user = User.new(params[:user])
-	  if @user.save
+    @user = User.new(params[:user])
+    if @user.save
       sign_in @user
-	    flash[:success] = "Welcome to Linux News!"
-	    redirect_to @user
-	  else
-	    render 'new'
-	  end
-	end
+      flash[:success] = "Welcome to Linux News!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 
   def edit
     @user = User.find(params[:id])
@@ -64,7 +64,7 @@ class UsersController < ApplicationController
     redirect_to(root_path) unless current_user.admin?
   end
 
-  def signined_user
-    redirect_to(root_path) if signed_in?
-  end
+ def prevent_signined_resignup
+   redirect_to(root_path) if signed_in?
+ end
 end
